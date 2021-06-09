be9f0904ba636ade0538a02a9ec0438b8661c832
uday-31 <77710794+uday-31@users.noreply.github.com>
2021-06-09 23:42:59 +0800

    update stats for MeanVariance

--- Portfolio.py
+++ Portfolio.py
@@ -9,18 +9,20 @@ from Miscellaneous import FetchData
 
 class MeanVariance:
 
-    def __init__(self,historicalReturns: pd.DataFrame,tickers: list=None,bounds: Union[tuple,list]=(0,1),
+    def __init__(self,historicalPrices: pd.DataFrame,tickers: list=None,bounds: Union[tuple,list]=(0,1),riskFreeRate: float=None,
     solver: str=None, solverOptions: dict=None,verbose: bool=False):
         """Constructor to instantiate the class based on the input parameters.
 
         Parameters
         ----------
-        historicalReturns : pd.DataFrame
-            DataFrame of historical returns for each ticker, with column name as name of ticker and index as timestamps
+        historicalPrices : pd.DataFrame
+            DataFrame of historical prices for each ticker, with column name as name of ticker and index as timestamps
         tickers : list, optional
             List of tickers of the assets in the portfolio, by default None
         bounds : Union[tuple,list]
             Minimum and maximum weight of each asset or a single pair if all weights are identical, (-1,1) if shorting is allowed, by default (0,1)
+        riskFreeRate : float, optional
+            Risk free rate, by default None
         solver : str, optional
             Name of solver, by default None. List of solvers: cp.installed_solvers()
         solverOptions : dict, optional
@@ -28,43 +30,42 @@ class MeanVariance:
         verbose : bool, optional
             Whether performance and debugging information should be printed, by default False
         """
-        expectedReturns = expected_returns.mean_historical_return(historicalReturns)
-        covarianceMatrix = risk_models.CovarianceShrinkage(historicalReturns).ledoit_wolf()
+        expectedReturns = expected_returns.mean_historical_return(historicalPrices)
+        covarianceMatrix = risk_models.CovarianceShrinkage(historicalPrices).ledoit_wolf()
         self.portfolio=pypfopt.EfficientFrontier(expectedReturns,covarianceMatrix,bounds,solver,verbose,solverOptions)
+        if riskFreeRate is None:
+            # TODO: implement risk-free rate for same time period as returns; implement dynamic rf-rate rather than static
+            self.riskFreeRate=FetchData().risk_free_rate().mean().values[0]
+        else:
+            self.riskFreeRate=riskFreeRate
         self.weights=None
 
-    def fit(self,riskFreeRate: float=None)->OrderedDict:
+    def fit(self)->dict:
         """Optimize the portfolio by maxizing the Sharpe Ratio, and return the tickers and their respective weights.
-
-        Parameters
-        ----------
-        riskFreeRate : float, optional
-            Risk free rate, by default None
 
         Returns
         -------
-        OrderedDict
+        dict
             Returns a dictionary with format {ticker:weight}
-        """
-        if riskFreeRate is None:
-            # TODO: implement risk-free rate for same time period as returns; implement dynamic rf-rate rather than static
-            riskFreeRate=FetchData().risk_free_rate().mean().values[0]
-        self.weights=self.portfolio.max_sharpe(riskFreeRate)
+        """ 
+        self.weights=dict(self.portfolio.max_sharpe(self.riskFreeRate))
         return self.weights
 
-    def stats():
-        # Get statistics for your portfolio here
-        # import from the statistics file or use portfolioopt
-        ...
+    def stats(self,verbose: bool=True)->tuple:
+        """Generate the expected annual return, annual volatility and Sharpe Ratio of the portfolio.
+
+        Parameters
+        ----------
+        verbose : bool, optional
+            Print the statistics, by default True
+
+        Returns
+        -------
+        tuple
+            Calculated statistics in the format (expected annual return, annual volatility, Sharpe Ratio)
+        """
+        stat=self.portfolio.portfolio_performance(verbose=verbose,risk_free_rate=self.riskFreeRate)
+        return stat
 
 class CPPI:
     ...
-
-
-ret=pd.DataFrame({"a":[1,2,3,4,5],"b":[5,6,7,8,9],"c":[2,4,6,8,10],"time":["2021-01-01","2021-01-02","2021-01-03","2021-01-04","2021-01-05"]})
-ret=ret.set_index('time')
-print(ret)
-pf = MeanVariance(ret)
-print(pf.portfolio.__dict__)
-weights=pf.fit()
-print(weights)
