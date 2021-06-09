6e7c499c8f1a25840ca2c3a4021b962957e58f48
uday-31 <77710794+uday-31@users.noreply.github.com>
2021-06-09 20:07:33 +0800

    add content for MeanVariance portfolio, minor typo fixes in Miscellaneous.py

--- Miscellaneous.py
+++ Miscellaneous.py
@@ -110,7 +110,7 @@ class Plot:
 
         return ax
 
-    def covariance_heatmap(covarianceMatrix:pd.Dataframe, showAssets:bool=True, plot:bool=False, **kwargs) -> ax:
+    def covariance_heatmap(covarianceMatrix:pd.DataFrame, showAssets:bool=True, plot:bool=False, **kwargs) -> ax:
         """The function returns a matplotlib axes object and computes the heatmap for the Covariance matrix
         of a given set of assets
 
@@ -136,7 +136,7 @@ class Plot:
 
         return ax
 
-    def correlation_heatmap(correlationMatrix:pd.Dataframe, showAssets:bool=True, plot:bool=False, **kwargs) -> ax:
+    def correlation_heatmap(correlationMatrix:pd.DataFrame, showAssets:bool=True, plot:bool=False, **kwargs) -> ax:
         """The function returns a matplotlib axes object and computes the heatmap for the correlationMatrix
         of a given set of assets
 
