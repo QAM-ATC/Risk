285939308bbeb4f4874a8fc1d9ee209915261d89
samaypanwar <69972858+samaypanwar@users.noreply.github.com>
2021-07-22 20:55:32 +0800

    Added print summary function and shifted old code to statistics

--- Statistics/summarize.py
+++ Statistics/summarize.py
@@ -1,160 +1,49 @@
-"Put summary function here that prints or returns a dataframe"
+from statistics import financial_ratios
+from statistics import annualize
+from statistics import statistics
 import pandas as pd
-from typing import Union
-import empyrical
-import numpy as np
-from scipy.stats import skew, kurtosis, skewtest, kurtosistest
 
-def calculate_skewness(price: Union[pd.DataFrame,pd.Series], test: bool = False, **kwargs) -> Union[float,pd.Series]:
-    """Calculates the skewness for a given set of prices
+def print_summary(price: pd.Series, **kwargs):
 
-    Parameters
-    ----------
-    price : Union[pd.DataFrame,pd.Series]
-        historical prices of a given security
-
-    Returns
-    -------
-    Union[float,pd.Series]
-        skewness for a given set of prices
+    # Fin ratios
     """
-    if test:
-        result = skewtest(price, **kwargs)
-        return result
+    1. Sharpe ratio
+    2. Sortino ratio
+    3. calmar ratio
+    4. Omega ratio
+    5. Tail Ratio
 
-    return skew(price)
+    Stats
+    1. Skewness
+    2. Kurtosis
+    3. Stability
+    4. Max Drawdown
+    5. Cumulative returns
 
+    Annualise
+    1. Returns
+    2. Vol
 
-def calculate_kurtosis(price: Union[pd.Series,pd.DataFrame], test: bool = False, **kwargs) -> Union[float,pd.Series]:
-    """Calculates the kurtosis for a given set of prices
-
-    Parameters
-    ----------
-    price : Union[pd.DataFrame,pd.Series]
-        historical prices of a given security
-
-    Returns
-    -------
-    Union[float,pd.Series]
-        kurtosis for a given set of prices
+    VaR
+    1. var
+    2. cvar
     """
 
-    if test:
-
-        result = kurtosistest(price, **kwargs)
-        return result
-
-    return kurtosis(price)
-
-def is_stable(price: pd.Series) -> float:
-    """Calculates stability for a given set of prices
-
-    Parameters
-    ----------
-    price : pd.Series
-       historical prices of a given security
-
-    Returns
-    -------
-    float
-       stability for a given set of prices
-    """
-    if isinstance(price, pd.DataFrame):
-        return price.apply(is_stable)
+    result = {}
+    for ratio in financial_ratios.__all__ :
+        result[ratio] = eval(f"financial_ratios.{ratio}(price, **kwargs)")
 
     returns = price.pct_change().dropna()
-    stability = empyrical.stats.stability_of_timeseries(returns)
-    return stability
 
-def maximum_drawdown(price: pd.Series) -> float:
-    """Calculates maximum drawdown for a given set of prices
+    for annual in annualize.__all__:
+        result[annual] = eval(f"annualize.{annual}(returns, **kwargs)")
 
-    Parameters
-    ----------
-    price : pd.Series
-        historical prices of a given security
+    for stat in statistics.__all__:
+        result[stat] = eval(f"statistics.{stat}(price, **kwargs)")
 
-    Returns
-    -------
-    float
-        maximum drawdown for a given set of prices
-    """
+    result['cumulative_returns'] = result['cumulative_returns'].iloc[-1, :]
 
-    maximum_drawdown.drawdowns = (price / price.cummax()) - 1
-    maximumDrawdown = (maximum_drawdown.drawdowns).min()
+    return pd.DataFrame.from_dict(result).T
 
-    return maximumDrawdown
-
-def cumulative_returns(price: Union[pd.DataFrame, pd.Series]) -> float:
-    """Calculates cumulative returns for a given set of prices
-
-    Parameters
-    ----------
-    price : Union[pd.DataFrame, pd.Series]
-        historical prices of a given security
-
-    Returns
-    -------
-    float
-        cumulative returns for a given set of prices
-    """
-    returns = price.pct_change().dropna()
-    cumReturns = empyrical.stats.cum_returns(returns)
-
-    return cumReturns
-
-def alpha(price: pd.Series, marketReturn: float, riskFreeRate: float = 0.0, periodsPerYear: Union[float, int] = 252) -> float:
-    """Calculates annualised alpha for a given set of prices, risk free rate and benchmark return (market return in CAPM)
-
-    Parameters
-    ----------
-    price : pd.Series
-        historical prices of a given security
-    riskFreeRate : float
-        given constant risk free rate throughout the period
-    marketReturn : float
-        daily noncumulative benchmark return throughout the period
-    periodsPerYear : Union[float, int]
-        periodicity of the returns data for purposes of annualising
-
-    Returns
-    -------
-    float
-        annualised alpha for a given set of prices, risk free rate and benchmark return (market return in CAPM)
-    """
-
-    raise NotImplementedError("Will do it later :P")
-    if isinstance(price, pd.DataFrame):
-        return price.apply(alpha, args = (marketReturn , riskFreeRate, periodsPerYear))
-
-    returns = price.pct_change().dropna()
-    a = empyrical.stats.alpha(returns, factor_returns = marketReturn, risk_free = riskFreeRate, annualization = periodsPerYear)
-
-    return a
-
-def beta(price: pd.Series, riskFreeRate: float, marketReturn: float, periodsPerYear: Union[float, int]) -> float:
-    """Calculates annualised beta for a given set of prices, risk free rate and benchmark return (market return in CAPM)
-
-    Parameters
-    ----------
-    price : pd.Series
-        historical prices of a given security
-    riskFreeRate : float
-        given constant risk free rate throughout the period
-    marketReturn : float
-        daily noncumulative benchmark return throughout the period
-    periodsPerYear : Union[float, int]
-        periodicity of the returns data for purposes of annualising
-
-    Returns
-    -------
-    float
-        annualised beta for a given set of prices, risk free rate and benchmark return (market return in CAPM)
-    """
-    #TO DO: take dataframe as price input
-    raise NotImplementedError("Will do it later :P")
-    r = price.pct_change().dropna()
-    b = empyrical.stats.beta(r, factor_returns = marketReturn, risk_free = riskFreeRate, annualisation = periodsPerYear)
-    return b
 
 
